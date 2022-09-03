2f4edc6e457e08ea312dcbac883aa99ea8220a5b
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-09-03 13:23:49 -0400

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Defines unittests for models/engine/file_storage.py
     Unittest classes:
-    TestFileStorage_instantiation
-    TestFileStorage_methods
+        TestFileStorage_instantiation
+        TestFileStorage_methods
 """
 import os
 import models
