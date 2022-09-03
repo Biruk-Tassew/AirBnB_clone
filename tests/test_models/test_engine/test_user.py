ed28ea987c10e70f5e2f87aa9b52e2166e80b4d0
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-09-03 13:25:52 -0400

    Update test_user.py

--- tests/test_models/test_engine/test_user.py
+++ tests/test_models/test_engine/test_user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines unittests for models/user.py
     Unittest classes:
-    TestUser_instantiation
+        TestUser_instantiation
     TestUser_save
     TestUser_to_dict
 """
